46fe2c3848515599e1bd977a482a0f0ac6e50e62
Fathir <fathirulfatih29@gmail.com>
2026-01-07 14:26:55 +0700

    add route crud

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiswaController;
 
 Route::get('/', [SiswaController::class, 'index']);
+Route::post('/store', [SiswaController::class, 'store']);
+Route::get('/edit/{id}', [SiswaController::class, 'edit']);
+Route::post('/update/{id}', [SiswaController::class, 'update']);
+Route::get('/delete/{id}', [SiswaController::class, 'destroy']);
