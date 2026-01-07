fd3ecad8a2d55d3cc63d30e8750ad9c5c798e23f
Fathir <fathirulfatih29@gmail.com>
2026-01-07 14:16:30 +0700

    add code siswacontroller

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SiswaController extends Controller
 {
-    //
+    public function index()
+    {
+        $siswas = DB::table('siswas')->get();
+        return view('siswa', compact('siswas'));
+    }
 }
+
