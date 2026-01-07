52d7bc62d72610f907f9e53376dddf26e11cb93d
Fathir <fathirulfatih29@gmail.com>
2026-01-07 14:20:00 +0700

    change route

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\SiswaController;
 
-Route::get('/siswa', [SiswaController::class, 'index']);
+Route::get('/', [SiswaController::class, 'index']);
