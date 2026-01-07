f486323f5d8811c5fd62731e5bada4e70607c286
Fathir <fathirulfatih29@gmail.com>
2026-01-07 14:31:38 +0700

    add create in route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiswaController;
 
 Route::get('/', [SiswaController::class, 'index']);
+Route::get('/create', [SiswaController::class, 'create']);
 Route::post('/store', [SiswaController::class, 'store']);
 Route::get('/edit/{id}', [SiswaController::class, 'edit']);
 Route::post('/update/{id}', [SiswaController::class, 'update']);
