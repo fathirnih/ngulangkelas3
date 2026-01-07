a16c3629f48ae458df70da9e15b4c05f8af3653e
Fathir <fathirulfatih29@gmail.com>
2026-01-07 14:28:34 +0700

    add crud to siswacontroller

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class SiswaController extends Controller
 {
@@ -10,6 +11,38 @@ class SiswaController extends Controller
     {
         $siswas = DB::table('siswas')->get();
         return view('siswa', compact('siswas'));
+    }
+
+    public function store(Request $request)
+    {
+        DB::table('siswas')->insert([
+            'nama' => $request->nama,
+            'kelas' => $request->kelas,
+        ]);
+
+        return redirect('/');
+    }
+
+    public function edit($id)
+    {
+        $siswa = DB::table('siswas')->where('id', $id)->first();
+        return view('edit', compact('siswa'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        DB::table('siswas')->where('id', $id)->update([
+            'nama' => $request->nama,
+            'kelas' => $request->kelas,
+        ]);
+
+        return redirect('/');
+    }
+
+    public function destroy($id)
+    {
+        DB::table('siswas')->where('id', $id)->delete();
+        return redirect('/');
     }
 }
 
